24bc84efe633fe33b97d2d00edfe370fc00b0fdd
Aedan-Paltridge-Nicholls <aedan432@gmail.com>
2023-11-03 19:33:50 +1300

    11/3/2023

---
+++ 106.2/AdminLoginView.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _106._2
+{
+    /// <summary>
+    /// Interaction logic for AdminLoginView.xaml
+    /// </summary>
+    public partial class AdminLoginView : Page
+    {
+        public class memberdata 
+        {
+            string number, name, phonenumbers, email, joindate, address;
+
+        } 
+
+        public AdminLoginView()
+        {
+            InitializeComponent();
+
+            string connectionString = "Server=localhost;Port=5432;UserId=postgres;Password=Nicholls2004;Database=106.2;";
+
+            using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    string sql = "SELECT * FROM Members"; // Replace with your SQL query
+                    NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(sql, con);
+                    DataTable dt = new DataTable();
+                    dataAdapter.Fill(dt);
+                    membersdatagrid.ItemsSource = dt.DefaultView; // Replace 'dataGrid' with your DataGrid's name
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+
+            
+        }
+        private void AddMemberBUTTON_Click(object sender, RoutedEventArgs e)
+        {
+            string comand = "  insert into members (number, name, phonenumbers, email, joindate, address) values (2, 'Krysta Rizziello', '581-231-2360', 'krizziello1@nhs.uk', '2022-11-25', '2890 1st Parkway');";
+
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
--- 106.2/MainWindow.xaml.cs
+++ 106.2/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Npgsql;
 namespace _106._2
 {
     /// <summary>
@@ -20,9 +20,38 @@ namespace _106._2
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
+            NpgsqlConnection connectionString = new NpgsqlConnection("Server=localhost;Port=5432;UserId=postgres;Password=Nicholls2004;Database=106.2;");
+
             InitializeComponent();
+
+        }
+
+        private void HomeButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+                                        
+        }
+
+        private void AdminBookButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void DuedateButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void MembersButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Main.Content = new AdminLoginView();
+
+        }
+
+        private void HomeButton_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+
         }
     }
 }
